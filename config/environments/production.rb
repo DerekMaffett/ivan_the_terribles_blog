da6d18d55edde501e052f0a6e577d45ee5b233aa
Derek Maffett <derekgmaffett@gmail.com>
2014-10-15 23:44:58 -0700

    change config for assets pipeline for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,8 @@ config.cache_store = :dalli_store,
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
